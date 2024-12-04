1b361e4be26537c8617da83fb297baa550804d54
Sam948-byte <techguy763@gmail.com>
2024-12-03 18:48:25 -0600

    I gotta do calc now

--- entropyCalc.py
+++ entropyCalc.py
@@ -2,17 +2,17 @@ import math
 import subprocess
 import json
 
-#ENSURE THAT ALL PATHS ARE MODIFIED TO ACCURATELY REFLECT LOCATIONS ON YOUR MACHINE
+# ENSURE THAT ALL PATHS ARE MODIFIED TO ACCURATELY REFLECT LOCATIONS ON YOUR MACHINE
 
 # List of different attacks
-hashcats = [
+HASHCATS = [
     [
         "hashcat",
         "-m",
         "99999",
         "/home/samf/gitClones/APResearch/resources/hashes/hashes.txt",
         "--outfile-format",
-        "1,2,4",
+        "1,4",
         "-o",
         "/home/samf/gitClones/APResearch/resources/hashes/out.txt",
         "-a",
@@ -26,7 +26,7 @@ hashcats = [
         "99999",
         "/home/samf/gitClones/APResearch/resources/hashes/hashes.txt",
         "--outfile-format",
-        "1,2,4",
+        "1,4",
         "-o",
         "/home/samf/gitClones/APResearch/resources/hashes/out.txt",
         "/home/samf/gitClones/APResearch/resources/wordlists/rockyou.txt",
@@ -34,11 +34,45 @@ hashcats = [
         "/home/samf/gitClones/APResearch/resources/rules/OneRuleToRuleThemStill.rule",
     ],  # Rock you w/ OneRuleToRuleThemAll
 ]
+GUESSES_PER_HASHCAT = [1, 2]  # TODO: Update with actual guesses per hashcat
 
 
 def calculate_guesses():
-    for hashcat in hashcats:
+    for hashcat in HASHCATS:
         subprocess.run(hashcat)
+
+        #TODO: Make this work
+        with open(
+            "/home/samf/gitClones/APResearch/resources/hashes/out.txt", mode="r"
+        ) as out_file:
+            out = out_file.read()
+
+        out = out.split("\n")
+
+        data = json.load(open("/home/samf/gitClones/APResearch/data/output.json"))
+
+        for datum in data:
+            for key in datum["Passwords"]:
+                guesses = int(datum["Passwords"][key]["Guesses"])
+                password = datum["Passwords"][key]["Password"]
+                cracked = False
+                for line in out:
+                    if password in line:
+                        guesses += int(line.split(":")[1])
+                        datum["Passwords"][key]["Guesses"] = guesses
+                        cracked = True
+                        break
+                datum["Passwords"][key]["Cracked"] = cracked
+                if not cracked:
+                    guesses += GUESSES_PER_HASHCAT[HASHCATS.index(hashcat)]
+                    datum["Passwords"][key]["Guesses"] = guesses
+
+        json_data = json.dumps(data, indent=4)
+
+        with open(
+            "/home/samf/gitClones/APResearch/data/output.json", mode="w"
+        ) as json_file:
+            json_file.write(json_data)
 
 
 def calculate_entropy(password):
@@ -74,25 +108,32 @@ def calculate_entropy(password):
 
 def main():
     data = json.load(open("/home/samf/gitClones/APResearch/data/output.json"))
-    
+
+    # clear hashes file
+    with open(
+        "/home/samf/gitClones/APResearch/resources/hashes/hashes.txt", mode="w"
+    ) as hash_file:
+        hash_file.write("")
+
     for datum in data:
         for key in datum["Passwords"]:
             password = datum["Passwords"][key]["Password"]
-            print(password)
             entropy = calculate_entropy(password)
             datum["Passwords"][key]["Entropy"] = entropy
-            with open("/home/samf/gitClones/APResearch/resources/hashes/hashes.txt", mode="a") as hash_file:
+            with open(
+                "/home/samf/gitClones/APResearch/resources/hashes/hashes.txt", mode="a"
+            ) as hash_file:
                 hash_file.write(password + "\n")
-            
-    
 
     calculate_guesses()
 
-
     json_data = json.dumps(data, indent=4)
 
-    with open("/home/samf/gitClones/APResearch/data/output.json", mode="w") as json_file:
+    with open(
+        "/home/samf/gitClones/APResearch/data/output.json", mode="w"
+    ) as json_file:
         json_file.write(json_data)
+
 
 if __name__ == "__main__":
     main()
